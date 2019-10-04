fda4b1a37e7ebefc9d20eb4e4571368d91b94e4a
JaySebagh <jaysebagh@gmail.com>
2019-10-04 09:03:26 -0700

    Completed oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Base Class for FlightVehicle, Starship, and GroundVehicle
+class Vehicle():
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+#Base Class for Airplane that inherits from Vehicle
+class Starship(FlightVehicle):
+    pass
+
+#Base Class for Car and Motorcycle that inherits from Base Class Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
